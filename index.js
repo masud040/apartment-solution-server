43d2f9a70d18c996db27fa7519707e8674b4a67f
masudo40 <masudinfo128@gamil.com>
2023-11-24 18:58:37 +0600

    set up a api end point for get user base agreement

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
-const cookieParser = require("cookie-parser");
+
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6hil8lf.mongodb.net/?retryWrites=true&w=majority`;
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,42 +21,49 @@ const client = new MongoClient(uri, {
 
 const apartmentCollection = client.db("apartmentDB").collection("apartments");
 const usersCollection = client.db("apartmentDB").collection("users");
+const agreementsCollection = client.db("apartmentDB").collection("agreements");
+
+// const verifyToken = (req, res, next) => {
+//   if (!req?.headers?.authorization) {
+//     return res.status(401).send("Unauthorized access");
+//   }
+//   const token = req?.headers?.authorization.split(" ")[1];
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send("Unauthorized access");
+//     }
+//     req.decode = decoded;
+
+//     next();
+//   });
+// };
 
 async function run() {
   try {
     await client.connect();
-
+    // JWT related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
-        expiresIn: "365d",
+        expiresIn: "1hr",
       });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        })
-        .send({ success: true });
+
+      res.send({ token });
     });
 
-    // Logout
-    app.get("/logout", async (req, res) => {
-      try {
-        res
-          .clearCookie("token", {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-          })
-          .send({ success: true });
-        console.log("Logout successful");
-      } catch (err) {
-        res.status(500).send(err);
-      }
+    // Role related api
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params?.email;
+      // if (email !== req.decode.email) {
+      //   return res.status(403).send({ message: "Forbidden" });
+      // }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const role = user?.role;
+      res.send({ role });
     });
-
+    // Users related api
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -80,6 +82,7 @@ async function run() {
       res.send(result);
     });
 
+    // apartments related api
     app.get("/apartments", async (req, res) => {
       const page = Number(req.query.page);
       const size = Number(req.query.size);
@@ -93,6 +96,20 @@ async function run() {
       const total = await apartmentCollection.estimatedDocumentCount();
 
       res.send({ apartments: result, total });
+    });
+
+    // agreement related api
+    app.get("/agreement/:email", async (req, res) => {
+      const query = { email: req.params?.email };
+
+      const result = await agreementsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/agreements", async (req, res) => {
+      const agreement = req?.body;
+      const result = await agreementsCollection.insertOne(agreement);
+      res.send(result);
     });
 
     app.all("*", (req, res, next) => {
